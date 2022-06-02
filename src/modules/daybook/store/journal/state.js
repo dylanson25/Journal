968be0fbfeb696268ab5702f661f18d2ce5a4716
Dylanson25 <dvillarreal0@ucol.mx>
2022-06-02 17:48:47 -0500

    New examples state

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,8 +1,27 @@
 /**
- * El state es reactivo y cuando cambia se notifiva a todos los componentes 
+ * El state es reactivo y cuando cambia se notifiva a todos los componentes
  * o a varios lugares donde esten escuchando los cambios del state
  */
 export default () => ({
- isLoding: true,
- entries: [1,2,3,4,5]
-})
+  isLoding: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      Date: new Date().toDateString(),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat suscipit dolorum mollitia. Dolor reprehenderit totam unde, iusto necessitatibus eos incidunt! Veniam esse quasi, excepturi ipsum odio similique. Corrupti, dolorem!",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      Date: new Date().toDateString(),
+      text: "Ipsum lorem dolor sit amet consectetur adipisicing elit. Harum fugiat suscipit dolorum mollitia. Dolor reprehenderit totam unde, iusto necessitatibus eos incidunt! Veniam esse quasi, excepturi ipsum odio similique. Corrupti, dolorem!",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      Date: new Date().toDateString(),
+      text: "Pato Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat suscipit dolorum mollitia. Dolor reprehenderit totam unde, iusto necessitatibus eos incidunt! Veniam esse quasi, excepturi ipsum odio similique. Corrupti, dolorem!",
+      picture: null,
+    },
+  ],
+});
