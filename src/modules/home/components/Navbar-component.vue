2bd156ade281d29fe7db47a7e353762866beb329
Dylanson25 <dvillarreal0@ucol.mx>
2022-07-20 20:58:36 -0500

    new component navbar to homeLayout

---
+++ src/modules/home/components/Navbar-component.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar bg-primary">
+    <a class="navbar-brand text-white">
+      <img
+        src="@/assets/logo.png"
+        alt="Vue logo"
+        height="24"
+        class="d-inline-block align-text-top mx-2"
+      />
+      My journal
+    </a>
+    <ul class="nav__items">
+        <li class="nav__item"><a @click="onLogin" class="item__navigate item__navigate--hover">Login</a></li>
+        <li class="nav__item">/</li>
+        <li class="nav__item"><a @click="onLogin" class="item__navigate item__navigate--hover">Register</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+
+export default {    
+  setup() {
+    const router = useRouter();
+    return {
+      onLogin: () => {
+        router.push({ name: "login" });
+      },
+      onRegister: () => {
+        router.push({ name: "register" });
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+    .nav__items{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        list-style: none;
+        padding: 0;
+        margin-right: 2rem;
+        width: 8rem;
+
+    }
+    .nav__item{
+        color: white;
+        
+    }
+    .item__navigate{
+        color: white;
+        font-weight: 600;
+        text-decoration: none;
+    }
+    .item__navigate:hover{
+        color: wheat;
+        cursor: pointer;
+        font-size: 1rem;
+        text-decoration: underline;
+    }
+</style>
--- src/modules/home/layouts/HomeLayout.vue
+++ src/modules/home/layouts/HomeLayout.vue
@@ -1,20 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Home view en proceso</h1>
-    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
-  </div>
+  <NavbarComponent />
+  <main class="home">
+    <div class="description">
+      <h1 class="description__title">The Best Journal Web</h1>
+      <q class="description__quote">
+        This is an application in which you can create a diary of events that
+        are saved with the date of the day, capable of searching by the text
+        that was written and adding a relevant image.
+      </q>
+    </div>
+  </main>
 </template>
 
 <script>
+import { defineAsyncComponent } from "vue";
 export default {
-  methods: {
-    goToDaybook() {
-      this.$router.push({ name: "no-entry" });
-    },
+  components: {
+    NavbarComponent: defineAsyncComponent(() =>
+      import("../components/Navbar-component.vue")
+    ),
+  },
+  setup() {
+    return {};
   },
 };
 </script>
 
 <style scoped>
+.home {
+  align-items: center;
+  background-color: white;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  padding: 2rem;
+}
+.description {
+  display: flex;
+  flex-wrap: wrap;
+  width: 45%;
+}
+.description__title {
+  color: black;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+.description__quote {
+  font-size: 20px;
+}
 </style>
